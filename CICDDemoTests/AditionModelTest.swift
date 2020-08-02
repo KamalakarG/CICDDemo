5ce858ed1829843c5fa3d19f005c2e725b285d97
KamalakarG <kamalakargoretta@gmail.com>
2020-08-02 08:11:29 +0530

    test success

--- CICDDemoTests/AditionModelTest.swift
+++ CICDDemoTests/AditionModelTest.swift
@@ -23,7 +23,7 @@ class AditionModelTest: XCTestCase {
     
     func testAddition() {
         let result = additionModel?.getSum(a: 3, b: 5)
-        XCTAssertEqual(result, 9, "sum of 3 and 5 should be 8")
+        XCTAssertEqual(result, 8, "sum of 3 and 5 should be 8")
     }
 
 }
